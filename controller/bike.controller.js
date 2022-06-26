3a3e7261b348c7af08ae19e3d540ead4143cffad
akash.dutta <dutta.akash1993official@gmail.com>
2022-06-26 12:17:55 +0530

    get bike by id, update bike, delete bike

--- controller/bike.controller.js
+++ controller/bike.controller.js
@@ -68,3 +68,91 @@ exports.GetAllBikesPagination = async (req, res) => {
 		});
 	}
 };
+
+/**
+ * !GetBikeById
+ * !Get
+ */
+exports.GetBikeById = async (req, res) => {
+	try {
+		const { id } = req.params;
+		if (!id) res.status(400).json({ error: 'Bike Id required' });
+		const bike = await Bike.findById(id);
+
+		if (!bike) res.status(404).json({ error: 'could not find bike' });
+		res.status(200).json({
+			status: 'Success',
+			bike,
+		});
+	} catch (err) {
+		res.status(400).json({
+			status: 'Error',
+			err: err.message,
+		});
+	}
+};
+
+/**
+ * !UpdateBike
+ * !Update
+ */
+
+exports.UpdateBike = async (req, res) => {
+	try {
+		// const userId = req.user.id;
+		const bike = await Bike.findById(req.params.id);
+
+		if (!bike) return res.status(404).json({ Error: 'Bike not found' });
+
+		// if (!isUserTheOwner(userId, region.owner)) {
+		// 	res.status(400).json({
+		// 		status: 'Failed, you are not the owner, you cant update',
+		// 	});
+		// }
+
+		const updatedBike = await bike.update(req.body, {
+			new: true,
+		});
+		if (!updatedBike) return res.status(404).json({ Error: 'Could not update bike' });
+		res.status(200).json({
+			msg: 'Bike updated successfully',
+			updatedBike,
+		});
+	} catch (err) {
+		res.status(400).json({
+			status: 'Error',
+			err: err.message,
+		});
+	}
+};
+
+/**
+ * !DeleteBike
+ * !Delete
+ */
+exports.DeleteBike = async (req, res) => {
+	try {
+		// const userId = req.user.id;
+		const bike = await Bike.findById(req.params.id);
+
+		if (!bike) return res.status(404).json({ Error: 'Bike not found' });
+
+		// if (!isUserTheOwner(userId, region.owner)) {
+		// 	res.status(400).json({
+		// 		status: 'Failed, you are not the owner, you cant delete',
+		// 	});
+		// }
+
+		const isDeleted = await bike.remove();
+		if (!isDeleted) return res.status(404).json({ Error: 'Could not delete bike' });
+		res.json({
+			msg: 'bike successfully deleted ',
+			id: req.params.id,
+		});
+	} catch (error) {
+		res.status(400).json({
+			status: 'Error',
+			err: err.message,
+		});
+	}
+};
--- routes/bike.router.js
+++ routes/bike.router.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const { AddBike, GetAllBikes, GetAllBikesPagination } = require('../controller/bike.controller');
+const {
+	AddBike,
+	GetAllBikes,
+	GetAllBikesPagination,
+	GetBikeById,
+	UpdateBike,
+	DeleteBike,
+} = require('../controller/bike.controller');
 
 router.post('/bike/add-bike', AddBike);
 router.get('/bike/all-bikes', GetAllBikes);
 router.get('/bike/all-bikes/pagination', GetAllBikesPagination);
+router.get('/bike/:id', GetBikeById);
+router.put('/bike/:id', UpdateBike);
+router.delete('/bike/:id', DeleteBike);
 module.exports = router;
